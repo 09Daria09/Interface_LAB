a2ee5578b7354d69da73b6102e643e2810877a2c
Daria <kukuruzadasha86@gmail.com>
2023-02-14 18:28:33 +0200

    add HomeWork

--- Interface_LAB/Program.cs
+++ Interface_LAB/Program.cs
@@ -8,14 +8,72 @@ namespace Interface_LAB
 {
     class Program
     {
-        class Array_ : IOutput, IMath, ISort
+        class Array_ : IOutput, IMath, ISort, ICalc, IOutput2, ICalc2
         {
             Random random = new Random();
-            int[] array = new int[5];
+            int[] array = new int[10];
 
             public float Avg()
             {
                 return (float)array.Average();
+            }
+
+            public int CountDistinct()
+            {
+                int count = 0;
+                int count1 = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for(int j = 0; j < array.Length; j++)
+                    {
+                        if(array[i] != array[j])
+                            count++;
+                    }
+                    if(count == array.Length - 1)
+                        count1++;
+                    count = 0;
+                }
+
+                return count1;
+            }
+
+            public int EqualToValue(int valueToCompare)
+            {
+                int count = 0;
+                for(int i = 0;i < array.Length; i++)
+                {
+                    if(array[i] == valueToCompare)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            public int Greater(int valueToCompare)
+            {
+                int count = 0;
+                for(int i =0; i < array.Length; i++)
+                {
+                    if(array[i] > valueToCompare)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            public int Less(int valueToCompare)
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] < valueToCompare)
+                    {
+                        count++;
+                    }
+                }
+                return count;
             }
 
             public int Max()
@@ -44,7 +102,7 @@ namespace Interface_LAB
             {
                 for (int i = 0; i < array.Length; i++)
                 {
-                    array[i] = random.Next(1, 9);
+                    array[i] = random.Next(1, 20);
                     Console.Write($"{array[i]}  ");
                 }
 
@@ -57,6 +115,28 @@ namespace Interface_LAB
                     Console.WriteLine($"{array[i]  }");
                 }
                 Console.WriteLine(info);
+            }
+
+            public void ShowEven()
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    if(array[i]% 2 == 0)
+                    {
+                        Console.Write($"{array[i]}  ");
+                    }
+                }
+            }
+
+            public void ShowOdd()
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] % 2 != 0)
+                    {
+                        Console.Write($"{array[i]}  ");
+                    }
+                }
             }
 
             public void SortAsc()
@@ -88,17 +168,38 @@ namespace Interface_LAB
             Array_ array = new Array_();
             array.Show();
             Console.WriteLine();
+
+            Console.WriteLine(array.CountDistinct());
+
+            Console.WriteLine(array.EqualToValue(3));
+
             Console.WriteLine(array.Min());
             Console.WriteLine();
+
             Console.WriteLine(array.Max());
             Console.WriteLine();
+
             Console.WriteLine(array.Search(1));
             Console.WriteLine();
+
             array.SortAsc();
             Console.WriteLine();
+
             array.SortDesc();
             Console.WriteLine();
+
             array.SortByParam(true);
+            Console.WriteLine();
+
+            Console.WriteLine(array.Greater(3));
+            Console.WriteLine(array.Less(2));
+            Console.WriteLine();
+
+            array.ShowEven();
+            Console.WriteLine();
+
+            array.ShowOdd();
+            Console.WriteLine();
 
         }
     }
